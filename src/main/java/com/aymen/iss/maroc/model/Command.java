6e4135a01d003826e6f0da2f2e84bc8f9d365aa3
Aymen-Kassab <aymankassab123@gmail.com>
2025-07-16 12:48:33 +0100

    3rd

---
+++ src/main/java/com/aymen/iss/maroc/model/Command.java
@@ -0,0 +1,17 @@
+package com.aymen.iss.maroc.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Command {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+    private String fullName;
+    private String productName;
+}
