d9d09b8f91f372ee962bf30240c3802f8f499a21
Aymen-Kassab <aymankassab123@gmail.com>
2025-07-16 14:08:45 +0100

    4th

--- src/main/java/com/aymen/iss/maroc/model/Command.java
+++ src/main/java/com/aymen/iss/maroc/model/Command.java
@@ -14,4 +14,51 @@ public class Command {
     private long id;
     private String fullName;
     private String productName;
+    private String price;
+    private String date;
+    private String status;
+
+   //Getters
+
+   public String getFullName(){
+       return this.fullName;
+   }
+
+   public String getProductName(){
+       return this.productName;
+   }
+
+   public String getPrice(){
+       return this.price;
+   }
+
+   public String getDate(){
+       return this.date;
+   }
+
+   public String getStatus(){
+       return this.status;
+   }
+
+   //Setters
+
+    public void setFullName(String fullName){
+       this.fullName = fullName;
+    }
+
+    public void setProductName(String productName){
+       this.productName = productName;
+    }
+
+    public void setPrice(String price){
+       this.price = price;
+    }
+
+    public void setDate(String date){
+       this.date = date;
+    }
+
+    public void setStatus(String status){
+       this.status = status;
+    }
 }
