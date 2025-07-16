17a04b026bfc5da9a42ea4be092e7c299b375d82
Aymen-Kassab <aymankassab123@gmail.com>
2025-07-16 17:35:32 +0100

    6th

--- src/main/java/com/aymen/iss/maroc/controller/ContactController.java
+++ src/main/java/com/aymen/iss/maroc/controller/ContactController.java
@@ -1,4 +1,30 @@
 package com.aymen.iss.maroc.controller;
 
+import com.aymen.iss.maroc.model.Contact;
+import com.aymen.iss.maroc.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/contact")
+@CrossOrigin(origins = "*") //Allow frontend to connect
 public class ContactController {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    //Post - save new contact
+    @PostMapping
+    public Contact createContact(@RequestBody Contact contact){
+        return contactRepository.save(contact);
+    }
+
+    //Get - get all contacts
+    @GetMapping
+    public List<Contact> getAllContact(){
+        return contactRepository.findAll();
+    }
+    
 }
